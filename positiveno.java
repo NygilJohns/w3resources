853a3980c320584df22be07c4300d270dfb6ec8e
NygilJohns <146074671+NygilJohns@users.noreply.github.com>
2023-09-27 14:33:35 +0530

    Add files via upload

---
+++ positiveno.java
@@ -0,0 +1,20 @@
+package w3worksample;
+
+import java.util.Scanner;
+
+public class positiveno {
+	public static void main(String [] args) {
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("Enter the number :");
+	int num=sc.nextInt();
+	if(num==0) 
+		System.out.println(num+" is neither positive nor negative");
+	else if(num>0)
+		System.out.println(num+" is positive");
+	else
+		System.out.println(num+" is negative");
+		
+	
+
+}}
